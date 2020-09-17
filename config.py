48f0b25f5932c5987b27224a50dd4d6e95e5eda1
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-09-17 19:37:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ import os
 class Config:
   ENV = bool(os.environ.get('ENV', False))
   if ENV:
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    APP_ID = os.environ.get('APP_ID')
-    API_HASH = os.environ.get('API_HASH')
+    BOT_TOKEN = os.environ.get('1325554549:AAGixaq5YaM4RcgIefJ3Ll9vSAMB7f8LuBQ')
+    APP_ID = os.environ.get('1452912')
+    API_HASH = os.environ.get('a15b90b055bae417d2c0a6f702e413b1')
     DATABASE_URL = os.environ.get('DATABASE_URL')
   else:
     BOT_TOKEN = '1325554549:AAGixaq5YaM4RcgIefJ3Ll9vSAMB7f8LuBQ' # Get it from https://t.me/BotFather
-    APP_ID = '1680362' # Get it from my.telegram.org/apps
-    API_HASH = '8cebfa99b9719c28a5e16fedaa0eeaad' # Get it from my.telegram.org/apps
+    APP_ID = '1452912' # Get it from my.telegram.org/apps
+    API_HASH = 'a15b90b055bae417d2c0a6f702e413b1' # Get it from my.telegram.org/apps
     DATABASE_URL = 'postgres://mdvgqgverqgesc:263a6b5b49f8539c4b1bb106ff7b300e5b5ebb3b46c3314fc57e8da9865c6457@ec2-34-192-122-0.compute-1.amazonaws.com:5432/d6c18273hu4a0r' # SQL Database URL / Heroku Postgres URL
 
 
